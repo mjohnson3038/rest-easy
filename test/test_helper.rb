2c03fc36e7002be955cc6e0c9d009d67ea1344e9
Madeleine Johnson <mjohnson3038@gmail.com>
2017-01-02 11:29:08 -0800

    Setting up Minitesting Environment

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,11 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+require 'simplecov'
+
+SimpleCov.start
+Rails.application.eager_load!
 
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.

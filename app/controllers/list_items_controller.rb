3036f57fb8dc0fc5ffb85fd1626c7b1eda38ac39
Madeleine Johnson <mjohnson3038@gmail.com>
2017-01-09 11:17:01 -0800

    Path to Edit ListItem Page

--- app/controllers/list_items_controller.rb
+++ app/controllers/list_items_controller.rb
@@ -1,5 +1,15 @@
 class ListItemsController < ApplicationController
 
+  def edit
+    # ADD CHECKING FOR USER
+    @list_item = ListItem.find(params[:id])
+
+  end
+
+  def update
+
+  end
+
   def create
     # @item = ListItem.new(list_item_params)
     #

7aa4a1a4e07e1c04defa4582cae653d4f37b46aa
Madeleine Johnson <mjohnson3038@gmail.com>
2017-01-11 15:04:21 -0800

    Can now create Guest Items without being dependent on the Guest_id, Woo

--- app/models/guest.rb
+++ app/models/guest.rb
@@ -1,4 +1,3 @@
 class Guest < ApplicationRecord
-  has_many :guest_items
   belongs_to :receipt
 end
--- app/models/guest_item.rb
+++ app/models/guest_item.rb
@@ -1,5 +1,3 @@
 class GuestItem < ApplicationRecord
   belongs_to :list_item
-  belongs_to :guest
-
 end

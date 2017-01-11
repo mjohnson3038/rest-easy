6e2dc8a9d922e8226998138bd7a87bd703b43e14
Madeleine Johnson <mjohnson3038@gmail.com>
2017-01-11 14:38:50 -0800

    WIP Splitting Check

--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,8 @@ Rails.application.routes.draw do
     resources :guests, only: [:index, :new, :create, :show]
   end
 
+
+  # Ge
   get "/receipts/guests/:id/split", to: "guests#split", as: "split_receipt"
+  post "/receipts/guests/:id/split", to: "guest_items#create", as: "post_guest_items"
 end

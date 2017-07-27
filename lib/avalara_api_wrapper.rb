6776da27e6fe6f0b4671f597bab6c4cfc5834f92
Madeleine Johnson <mjohnson3038@gmail.com>
2017-07-27 14:38:35 -0700

    Avalara Tax API has changed its user terms, need to seek alternate sales tax api, currently hardcoded to 6.25

--- lib/avalara_api_wrapper.rb
+++ lib/avalara_api_wrapper.rb
@@ -6,21 +6,25 @@ class AvalaraApiWrapper
   COUNTRY = "usa"
 
   def self.sales_tax(zip)
-    url = BASE_URL + "country=#{COUNTRY}" + "&postal=#{zip}" + "&apikey=#{KEY}"
-    puts url
-
-    data = HTTParty.get(url)
-    puts "data" + data.to_s
-    if data["totalRate"]
-      puts "YYYESS"
-      tax = data["totalRate"]
-      puts data["totalRate"]
-    else
-      puts "NOOOO"
-      puts data["totalRate"]
-      tax = 0
-    end
+    # url = BASE_URL + "country=#{COUNTRY}" + "&postal=#{zip}" + "&apikey=#{KEY}"
+    # # puts url
+    # #
+    # byebug
+    #
+    # data = HTTParty.get(url)
+    # puts "data" + data.to_s
+    # if data["totalRate"]
+    #   puts "YYYESS"
+    #   tax = data["totalRate"]
+    #   puts data["totalRate"]
+    # else
+    #   puts "NOOOO"
+    #   puts data["totalRate"]
+    #   tax = 0
+    # end
     # return tax
+    tax = 6.25
+
     return tax
 
   end
